a0ee9a0b71cb151419d279aa2ce2e5c58ff70f46
kramarajan <ram.sjc05@gmail.com>
2020-02-20 09:38:33 +0530

    DB First Sample

---
+++ DBFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            foreach (var item in courses)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
